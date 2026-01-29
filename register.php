cb982862380806c74dd8cc2c87f37167c3ff33f8
Santosh02411 <santoshmadannavar@gmail.com>
2026-01-30 00:36:20 +0530

    updated

---
+++ register.php
@@ -0,0 +1,10 @@
+<?php
+// Database connection
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "exam_proctoring";
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+?>
