e68620c13134497ab6723bb76ca3d15529197b15
Santosh02411 <santoshmadannavar@gmail.com>
2026-01-30 00:36:46 +0530

    updated

--- register.php
+++ register.php
@@ -6,5 +6,14 @@ $password = "";
 $dbname = "exam_proctoring";
 
 $conn = new mysqli($servername, $username, $password, $dbname);
+if ($conn->connect_error) {
+    die("DB Connection failed: " . $conn->connect_error);
+}
+
+
+
+
+
+
 
 ?>
