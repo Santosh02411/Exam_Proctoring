459bfca85a43c22c60c3517a43c67241bf9caf03
Santosh02411 <santoshmadannavar@gmail.com>
2026-01-30 00:37:16 +0530

    updated

--- register.php
+++ register.php
@@ -9,9 +9,15 @@ $conn = new mysqli($servername, $username, $password, $dbname);
 if ($conn->connect_error) {
     die("DB Connection failed: " . $conn->connect_error);
 }
+// Handle signup
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $name     = $_POST['name'];
+    $email    = $_POST['email'];
+    $phone    = $_POST['phone'];
+    $role     = $_POST['role'];  // NEW: role from form
+    $password = password_hash($_POST['password'], PASSWORD_BCRYPT);
 
-
-
+}
 
 
 
