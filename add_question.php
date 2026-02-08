2bb681947ae838b68a06875868b94e29b5aba6c0
Santosh02411 <santoshmadannavar@gmail.com>
2026-02-08 23:09:50 +0530

    updated

--- add_question.php
+++ add_question.php
@@ -1,4 +1,13 @@
 <?php
+session_start();
+if (!isset($_SESSION['user_id']) || $_SESSION['role'] !== 'admin') die("Access denied");
 
+$conn = new mysqli("localhost","root","","exam_proctoring");
+if ($conn->connect_error) die("DB err: ".$conn->connect_error);
 
+$test_db_id = isset($_GET['test_db_id']) ? (int)$_GET['test_db_id'] : null;
+if (!$test_db_id) {
+    echo "Provide test_db_id";
+    exit;
+}
 ?>
