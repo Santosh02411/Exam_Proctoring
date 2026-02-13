c50969db14aff7b12041b808ace51b3010455de9
Santosh02411 <santoshmadannavar@gmail.com>
2026-02-14 03:47:51 +0530

    updated

--- add_question.php
+++ add_question.php
@@ -213,7 +213,11 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     const marks = parseInt(marksEl.value) || 0;
 
   }
-
+    if (marks <= 0){
+      alert('Marks should be at least 1.');
+      marksEl.focus();
+      return false;
+    }
 
 
 
