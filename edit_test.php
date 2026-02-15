9ba3403dab59635285e7dfbf98881ae5dadad65c
Santosh02411 <santoshmadannavar@gmail.com>
2026-02-16 03:30:54 +0530

    updated

---
+++ edit_test.php
@@ -0,0 +1,15 @@
+<?php
+session_start();
+if (!isset($_SESSION['user_id']) || $_SESSION['role'] !== 'admin') {
+    header("Location: login.html"); exit;
+}
+$conn = new mysqli("localhost","root","","exam_proctoring");
+if ($conn->connect_error) die("DB err: ".$conn->connect_error);
+
+// fetch test id
+$test_id = isset($_GET['test_id']) ? (int)$_GET['test_id'] : 0;
+if (!$test_id) {
+    die("Missing test_id");
+}
+
+?>
