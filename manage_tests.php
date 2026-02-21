421384b40e6530ade195449840942af6308a6f44
Santosh02411 <santoshmadannavar@gmail.com>
2026-02-22 00:12:13 +0530

    updated

--- manage_tests.php
+++ manage_tests.php
@@ -3,6 +3,9 @@ session_start();
 if (!isset($_SESSION['user_id']) || $_SESSION['role'] !== 'admin') {
     header("Location: login.html"); exit;
 }
+$conn = new mysqli("localhost","root","","exam_proctoring");
+if ($conn->connect_error) die("DB err: ".$conn->connect_error);
+
 
 
 ?>
