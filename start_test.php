1660c36f62208dfc00622dba072207b0eca6d3a8
Santosh02411 <santoshmadannavar@gmail.com>
2026-03-05 01:11:21 +0530

    updated

--- start_test.php
+++ start_test.php
@@ -10,4 +10,12 @@ $test_id = isset($_GET['test_id']) ? (int)$_GET['test_id'] : 0;
 if (!$test_id) die("Invalid test");
 
 $attempt_token = bin2hex(random_bytes(12));
+
+
+$stmt = $conn->prepare("SELECT * FROM test_eligibility WHERE test_id=? AND student_id=?");
+$stmt->bind_param("ii", $test_id, $uid);
+$stmt->execute();
+$stmt->store_result();
+if ($stmt->num_rows === 0) die("You are not eligible for this test");
+
 ?>
